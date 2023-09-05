64cdc18bc99c2cfa8f57cdd112e22e0d6e5da642
ksalustiano <kermes.salustiano@gmail.com>
2023-09-05 12:35:53 -0300

    feat: create Controllers

---
+++ src/main/java/com/kermes/apiecommerce/Controllers/AddressController.java
@@ -0,0 +1,43 @@
+package com.kermes.apiecommerce.Controllers;
+
+import com.kermes.apiecommerce.DTOs.AddressCreateDTO;
+import com.kermes.apiecommerce.DTOs.AddressResponseDTO;
+import com.kermes.apiecommerce.DTOs.Mappers.AddressMapper;
+import com.kermes.apiecommerce.Entities.Address;
+import com.kermes.apiecommerce.Services.AddressService;
+import com.kermes.apiecommerce.Services.CepService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/address")
+public class AddressController {
+    private final  CepService cepService;
+    private final AddressService addressService;
+
+   @GetMapping("/{cep}")
+   public AddressResponseDTO findCep(@Valid @PathVariable String cep) {
+
+        return cepService.consultarCep(cep);
+    }
+
+    @PostMapping()
+    public ResponseEntity<AddressResponseDTO> criar(@Valid @RequestBody AddressCreateDTO addr){
+        Address endereco = addressService.criar(addr);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(AddressMapper.toDTO(endereco));
+
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AddressResponseDTO>> getAll(){
+        List<Address> addresses = addressService.buscarTodos();
+        return ResponseEntity.ok(AddressMapper.toListDTO(addresses));
+    }
+}
---
+++ src/main/java/com/kermes/apiecommerce/Controllers/ClientController.java
@@ -0,0 +1,43 @@
+package com.kermes.apiecommerce.Controllers;
+
+import com.kermes.apiecommerce.DTOs.ClientCreateDTO;
+import com.kermes.apiecommerce.DTOs.ClientReponseDTO;
+import com.kermes.apiecommerce.DTOs.Mappers.ClientMapper;
+import com.kermes.apiecommerce.Entities.Client;
+import com.kermes.apiecommerce.Services.ClientService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/clients")
+public class ClientController {
+
+    private final ClientService clientService;
+
+
+    @PostMapping()
+    public ResponseEntity<ClientReponseDTO> criar(@Valid @RequestBody ClientCreateDTO cli){
+        Client user = clientService.criar(cli);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(ClientMapper.toDTO(user));
+
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<ClientReponseDTO> getbyEmail(@PathVariable String email){
+        Client client = clientService.buscarPorEmail(email);
+        return ResponseEntity.ok(ClientMapper.toDTO(client));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ClientReponseDTO>> getAll(){
+        List<Client> clients = clientService.buscarTodos();
+        return ResponseEntity.ok(ClientMapper.tolistDTO(clients));
+    }
+}
